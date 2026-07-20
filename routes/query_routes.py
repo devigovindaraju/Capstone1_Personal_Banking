1ba187762c7161c3fdf807e24824a6c335ee8f5b
Saumya <saumyasheo1102@gmail.com>
2026-07-20 06:20:16 +0000

    Adding fastAPI endpoints

--- routes/query_routes.py
+++ routes/query_routes.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 
 from services.query_services import process_query
 
-router = APIRouter()
+router = APIRouter(prefix="/api/v1/advisor")
 
 
-@router.post("/query")
+@router.post("/")
 def query(request: dict):
     return process_query(request)
--- routes/upload_routes.py
+++ routes/upload_routes.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, UploadFile, File
 from services.upload_services import upload_document
 
-router = APIRouter()
+router = APIRouter(prefix="/api/v1/documents")
 
 
-@router.post("/upload")
+@router.post("/")
 async def upload(file: UploadFile = File(...)):
     response = await upload_document(file)
     return response
